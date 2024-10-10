06ba609de4bc8f0bf292c44e91bcb53adb01182a
Brayan <btellez948@gmail.com>
2024-10-09 20:10:52 -0600

    Cambios al main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import estructura.ColaSimple;
 import proceso.*;
 
 public class Main {
     public static void main(String[] args) {
         Proceso p1 = new Proceso("P1", 1, 10, 10, 0);
-        ListaProcesos lista = new ListaProcesos();
-        lista.insertarProceso(p1);
+        ColaSimple<Proceso> lista = new ColaSimple<>();
+        lista.insertar(p1);
 
         System.out.println("Hello");
         System.out.println("Lista de procesos listos: \n" + lista);
